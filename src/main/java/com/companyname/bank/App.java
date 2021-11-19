7823d423f3f6baca1a80edc33bb942455d58e619
Vihar.r.s <88980614+vihar10452@users.noreply.github.com>
2021-11-19 22:35:23 +0530

    Update App.java

--- src/main/java/com/companyname/bank/App.java
+++ src/main/java/com/companyname/bank/App.java
@@ -8,6 +8,6 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World! This is a new edit2" );
+        System.out.println( "Hello World! This is a new edit3" );
     }
 }
